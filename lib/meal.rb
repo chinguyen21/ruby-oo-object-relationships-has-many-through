012d1c3f63b9fdfe9ebdd9d3d61e420201a9a689
chinguyen21 <nguyen.thimai@northeastern.edu>
2020-11-21 16:37:26 -0500

    commit tree ebcdaf4cc8110b28502727049554277640970d2e

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,18 @@
 class Meal
+  attr_reader :total, :tip, :waiter, :customer
 
+  @@all = []
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @total = 0
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
 end
